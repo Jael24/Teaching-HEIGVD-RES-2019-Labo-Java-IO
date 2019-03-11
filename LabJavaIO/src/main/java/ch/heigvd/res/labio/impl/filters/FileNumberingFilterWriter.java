ed6bc8ceac8b14fcfb6b9a65f8613c85e8667585
Jael Dubey <jael.dubey@heig-vd.ch>
2019-03-11 20:42:15 +0100

    Implementation du filtre numero de ligne et tabulation

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -17,6 +17,10 @@ import java.util.logging.Logger;
  */
 public class FileNumberingFilterWriter extends FilterWriter {
 
+  int numeroLigne = 1;
+  boolean debutFichier = true;
+  boolean attenteWindows = false;
+
   private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
 
   public FileNumberingFilterWriter(Writer out) {
@@ -25,17 +29,83 @@ public class FileNumberingFilterWriter extends FilterWriter {
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    String tab = "\t";
+    boolean chaineAjoutee = false;
+    StringBuilder result = new StringBuilder();
+
+    if (debutFichier) {
+       result.append(numeroLigne++).append(tab);
+      debutFichier = false;
+    }
+
+    int longueurChaine = len + off;
+    int i;
+
+    for(i = off; i < longueurChaine; ++i) {
+      // si la longueur est egale a i+1, ne va pas tester le charAt(i+1) (en vertue du &&)
+      if (str.charAt(i) == '\r' && str.charAt(i+1) == '\n') {
+        result.append(str.substring(off, i + 2)).append(numeroLigne++).append(tab);
+        off = i + 2;
+        ++i;
+        if (off > len)
+          --off;
+        chaineAjoutee = true;
+      }
+      else if ((str.charAt(i) == '\n'  || (str.charAt(i) == '\r' && str.charAt(i+1) != '\n'))) {
+        result.append(str.substring(off, i + 1)).append(numeroLigne++).append(tab);
+        off = i + 1;
+        if (off > len)
+          --off;
+        chaineAjoutee = true;
+      }
+    }
+
+    if (str.charAt(i-1) != '\n' || !chaineAjoutee)
+      result.append(str.substring(off,i));
+
+    super.write(result.toString(),0,result.length());
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    for(int i = off; i < off+len; ++i){
+        write(cbuf[i]);
+    }
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    if(debutFichier) {
+      super.write(Integer.toString(numeroLigne++) + "\t", 0, 2);
+      debutFichier = false;
+    }
+
+    if(c == 10) {
+        String valeur = Integer.toString(numeroLigne++);
+        if (attenteWindows) {
+            super.write(13);
+            super.write(10);
+            super.write(valeur,0,valeur.length());
+            super.write(9); // 9 = '\t'
+            attenteWindows = false;
+            return;
+        }
+      super.write(c);
+      super.write(valeur,0,valeur.length());
+      super.write(9); // 9 = '\t'
+    } else if (c == 13) {
+        attenteWindows = true;
+    }
+    else {
+        if(attenteWindows) {
+            super.write(13);
+            super.write(Integer.toString(numeroLigne),0,Integer.toString(numeroLigne).length());
+            super.write(9); // 9 = '\t'
+            attenteWindows = false;
+        }
+      super.write(c);
+      attenteWindows = false;
+    }
   }
 
 }
