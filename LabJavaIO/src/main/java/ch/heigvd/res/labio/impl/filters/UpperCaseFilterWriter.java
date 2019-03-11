59b35eca718f66519d083817c85b3600c3a6293c
Jael Dubey <jael.dubey@heig-vd.ch>
2019-03-11 13:34:19 +0100

    Implementation du filtre majuscule

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/UpperCaseFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/UpperCaseFilterWriter.java
@@ -16,17 +16,24 @@ public class UpperCaseFilterWriter extends FilterWriter {
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    super.write(str.toUpperCase(),off,len);
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    for(int i = 0; i < cbuf.length; ++i) {
+      if(cbuf[i] <= 122 && cbuf[i] >= 97)
+        cbuf[i] = (char)(cbuf[i] - 32);
+    }
+    super.write(cbuf,off,len);
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    if(c <= 122 && c >= 97)
+      super.write(c - 32);
+    else
+      super.write(c);
   }
 
 }
