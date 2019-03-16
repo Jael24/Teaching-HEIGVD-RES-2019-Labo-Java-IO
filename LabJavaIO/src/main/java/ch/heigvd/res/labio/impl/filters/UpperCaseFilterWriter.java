4192928e97f886ce609459f82b2996ef263f6f74
Jael Dubey <jael.dubey@heig-vd.ch>
2019-03-16 14:16:16 +0100

    Revert "Implementation du filtre majuscule"
    This reverts commit 59b35eca718f66519d083817c85b3600c3a6293c.

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/UpperCaseFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/UpperCaseFilterWriter.java
@@ -16,24 +16,17 @@ public class UpperCaseFilterWriter extends FilterWriter {
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    super.write(str.toUpperCase(),off,len);
+    throw new UnsupportedOperationException("The student has not implemented this method yet.");
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    for(int i = 0; i < cbuf.length; ++i) {
-      if(cbuf[i] <= 122 && cbuf[i] >= 97)
-        cbuf[i] = (char)(cbuf[i] - 32);
-    }
-    super.write(cbuf,off,len);
+    throw new UnsupportedOperationException("The student has not implemented this method yet.");
   }
 
   @Override
   public void write(int c) throws IOException {
-    if(c <= 122 && c >= 97)
-      super.write(c - 32);
-    else
-      super.write(c);
+    throw new UnsupportedOperationException("The student has not implemented this method yet.");
   }
 
 }
